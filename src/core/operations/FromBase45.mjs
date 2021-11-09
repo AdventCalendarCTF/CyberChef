35437ae96ef5b19aa2a0479cf7792f0d8117c2ab
cdeletre <cyril.deletre@gmail.com>
2021-11-09 10:29:29 +0100

    Fixing last triplet processing

--- src/core/operations/FromBase45.mjs
+++ src/core/operations/FromBase45.mjs
@@ -58,7 +58,12 @@ class FromBase45 extends Operation {
                 throw new OperationError(`Triplet too large: '${triple.join("")}'`);
             }
 
-            res.push(b>>8);
+            if (len(triple) > 2):
+              # The last triple may only have 2 bytes so we push the MSB when we got 3 bytes
+              # Pushing MSB
+              res.push(b>>8);
+
+            # Pushing LSB
             res.push(b&0xff);
 
         }
